8e2a1128a29d2104f2e657564539024552aa28a6
Ciprian Dorin Craciun <ciprian@volution.ro>
2013-11-07 17:12:09 +0200

    Add usual scripts and update startup and configuration code

---
+++ scripts/run-component.bash
@@ -0,0 +1,48 @@
+#!/dev/null
+
+set -x
+
+_identifier="${1:-00000000bdebd8cfcd967b57cfbe7c81dc392525}"
+_fqdn="${mosaic_node_fqdn:-mosaic-0.loopback.vnet}"
+
+if test -n "${mosaic_component_temporary:-}" ; then
+	_tmp="${mosaic_component_temporary:-}"
+elif test -n "${mosaic_temporary:-}" ; then
+	_tmp="${mosaic_temporary}/components/${_identifier}"
+else
+	_tmp="/tmp/mosaic/components/${_identifier}"
+fi
+
+_run_bin="${_applications_elf}/me2cp.elf"
+_run_env=(
+		mosaic_component_identifier="${_identifier}"
+		mosaic_component_temporary="${_tmp}"
+		mosaic_node_fqdn="${_fqdn}"
+		transcript_level=information
+)
+
+if test "${_identifier}" != 00000000bdebd8cfcd967b57cfbe7c81dc392525 ; then
+	if ! test "${#}" -gt 2 ; then
+		echo "[ee] invalid arguments; aborting!" >&2
+		exit 1
+	fi
+	_run_args=(
+			component "${_identifier}"
+			"${@:2}"
+	)
+else
+	if ! test "${#}" -eq 0 ; then
+		echo "[ee] invalid arguments; aborting!" >&2
+		exit 1
+	fi
+	_run_args=(
+			standalone
+	)
+fi
+
+mkdir -p -- "${_tmp}"
+cd -- "${_tmp}"
+
+exec env "${_run_env[@]}" "${_run_bin}" "${_run_args[@]}"
+
+exit 1
--- sources/mosaic-components-me2cp/main.go
+++ sources/mosaic-components-me2cp/main.go
@@ -7,22 +7,74 @@ import "fmt"
 import "io"
 import "net"
 import "os"
+import "strconv"
 import "sync"
+
 
 import "vgl/transcript"
 
 
 func main () () {
 	
-	var _controllerEndpointUrl = defaultControllerEndpointUrl
-	var _channelEndpointIp string = defaultChannelEndpointIp
-	var _channelEndpointPort uint16 = defaultChannelEndpointPort
-	var _channelInboundStream *os.File = os.Stdin
-	var _channelOutboundStream *os.File = os.Stdout
+	var _componentIdentifier string
+	var _channelEndpointIp string
+	var _channelEndpointPort uint16
+	var _channelInboundStream *os.File
+	var _channelOutboundStream *os.File
+	var _controllerUrl string
+	var _bundle string
 	
 	_transcript := packageTranscript
 	
 	_transcript.TraceInformation ("initializing the ME2-based component proxy...")
+	
+	_arguments := os.Args
+	if len (_arguments) < 1 {
+		_transcript.TraceError ("invalid arguments (expected at least one); aborting!")
+		os.Exit (1)
+	}
+	switch _arguments[1] {
+		
+		case "component" :
+			if len (_arguments) != 7 {
+				_transcript.TraceError ("invalid component arguments (expected only the identifier); aborting!")
+				os.Exit (1)
+			}
+			_componentIdentifier = _arguments[2]
+			_channelEndpointIp = _arguments[5]
+			if _port, _error := strconv.ParseUint (_arguments[6], 10, 16); _error != nil {
+				_transcript.TraceError ("invalid channel edpoint ip; aborting!")
+				os.Exit (1)
+			} else {
+				_channelEndpointPort = uint16 (_port)
+			}
+			_controllerUrl = _arguments[4]
+			_bundle = _arguments[3]
+			_channelInboundStream = os.Stdin
+			_channelOutboundStream = os.Stdout
+		
+		case "standalone" :
+			if len (_arguments) != 2 {
+				_transcript.TraceError ("invalid standalone arguments (expected no others)")
+				os.Exit (1)
+			}
+			_transcript.TraceError ("although standalone stdio is still piped...")
+			_componentIdentifier = ""
+			_channelEndpointIp = defaultChannelEndpointIp
+			_channelEndpointPort = defaultChannelEndpointPort
+			_controllerUrl = defaultControllerUrl
+			_bundle = defaultBundle
+			_channelInboundStream = os.Stdin
+			_channelOutboundStream = os.Stdout
+		
+		default :
+			_transcript.TraceError ("invalid component mode `%s`; aborting!", _arguments[1])
+			os.Exit (1)
+	}
+	
+	if _componentIdentifier != "" {
+		_transcript.TraceInformation ("  * using the identifier `%s`;", _componentIdentifier)
+	}
 	
 	_transcript.TraceInformation ("preparing the channel endpoint accepter...")
 	var _accepter net.Listener
@@ -39,8 +91,12 @@ func main () () {
 	}
 	_transcript.TraceInformation ("  * using the endpoint `%s:%d`;", _channelEndpointIp, _channelEndpointPort)
 	
-	_transcript.TraceInformation ("preparing the the ME2 container...")
-	_transcript.TraceInformation ("  * using the endpoint `%s`;", _controllerEndpointUrl)
+	_transcript.TraceInformation ("preparing the the ME2 controller...")
+	_transcript.TraceInformation ("  * using the endpoint `%s`;", _controllerUrl)
+	_transcript.TraceInformation ("  * using the bundle `%s`;", _bundle)
+	
+	_transcript.TraceInformation ("creating the the ME2 container...")
+	_transcript.TraceInformation ("!!! not implemented yet !!!!")
 	
 	_transcript.TraceInformation ("waiting for a channel connection...")
 	var _connection net.Conn
@@ -75,6 +131,7 @@ func main () () {
 
 var packageTranscript = transcript.NewPackageTranscript ()
 
-var defaultControllerEndpointUrl = "http://127.0.0.1:xxx/"
+var defaultBundle = "none"
+var defaultControllerUrl = "http://127.0.0.1:8089/"
 var defaultChannelEndpointIp = "127.0.0.1"
 var defaultChannelEndpointPort uint16 = 24704
